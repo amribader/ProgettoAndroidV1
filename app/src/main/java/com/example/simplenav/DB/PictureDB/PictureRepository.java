63a36f462edac5ce1731872408c46153334f2d83
amribader <91694262+amribader@users.noreply.github.com>
2023-01-26 22:17:02 +0100

    inizio lavoro con db

--- app/src/main/java/com/example/simplenav/DB/PictureDB/PictureRepository.java
+++ app/src/main/java/com/example/simplenav/DB/PictureDB/PictureRepository.java
@@ -1,11 +1,18 @@
 package com.example.simplenav.DB.PictureDB;
 
+import androidx.room.Room;
+
+import com.example.simplenav.DB.AppDatabase;
+
 public class PictureRepository {
 
     public static void getPicture(Sid sid, int pversion, PictureDBListener pictureDBListener, ErrorPictureDBListener errorPictureDBListener){
         //controlla sul db se ce nella callback
             //nella collaback se ce chiama listenerr con immagine chimata
             //se non ce fai la chiamata di rete
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name")
+                .build();
 
     }
 
